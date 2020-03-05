b3f40f6708eb8888f6f3dc2de9281ba4f66d00d0
Alex <radionovalek@yandex.ru>
2020-03-05 09:01:08 +0700

    add mvp styles

--- src/app/movie-list/movie-list.component.ts
+++ src/app/movie-list/movie-list.component.ts
@@ -2,18 +2,17 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router'
 
 @Component({
-  selector: 'app-movie-list',
-  templateUrl: './movie-list.component.html',
-  styleUrls: ['./movie-list.component.css']
+    selector: 'app-movie-list',
+    templateUrl: './movie-list.component.html',
+    styleUrls: ['./movie-list.component.css']
 })
 export class MovieListComponent implements OnInit {
 
-  constructor(private router: Router) { }
+    constructor(private router: Router) { }
 
-  ngOnInit(): void {
-  }
-  goToPostsPage() {
-    this.router.navigate(['/'])
-  }
-
+    ngOnInit(): void {
+    }
+    goToPostsPage() {
+        this.router.navigate(['/'])
+    }
 }
