0945e3e2b88db06aa61f954d19cffa2f32670961
Alex <radionovalek@yandex.ru>
2020-03-06 09:17:06 +0700

    add  movie.service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MovieService } from './movie.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 
-export class AppComponent {
-  title = 'SCMovietest';
+export class AppComponent implements OnInit {
+    title = 'SCMovietest';
+
+    constructor(private movieService: MovieService) { }
+
+    ngOnInit() {
+        console.log(this.movieService)
+        this.movieService.getMovies()
+        .subscribe(movies => {
+            console.log(movies);
+
+        });
+    }
 }
--- src/app/movie.service.ts
+++ src/app/movie.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, Subject, BehaviorSubject } from 'rxjs'
 
+export interface Movie {
+    popularity: number,
+    vote_count: number,
+    video?: boolean,
+    poster_path: string,
+    id: number,
+    adult?: false,
+    backdrop_path: string,
+    original_language?: string,
+    original_title?: string,
+    genre_ids: [number],
+    title: string,
+    vote_average: number,
+    overview: string,
+    release_date: void  // hz object date ?
+}
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class MovieService {
+    movies: Movie[];
 
-  constructor() { }
+    constructor(private http: HttpClient) {}
+
+    getMovies(): Observable<Movie[]> {
+        return this.http.get<Movie[]>('https://api.themoviedb.org/3/movie/now_playing?api_key=07223f1ae4f3155a8e7eadc55a5431eb')
+    }
 }
+
+
