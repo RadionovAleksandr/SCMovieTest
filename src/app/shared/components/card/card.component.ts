dfaa5f47e11cec663e192ff2ac687af7e854b59e
Alex <radionovalek@yandex.ru>
2020-03-22 18:46:30 +0700

    start add adaptive

--- src/app/shared/components/card/card.component.ts
+++ src/app/shared/components/card/card.component.ts
@@ -1,12 +1,10 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Movie } from '../../movie.service';
-
 @Component({
     selector: 'app-card',
     templateUrl: './card.component.html',
     styleUrls: ['./card.component.scss'],
 })
-
 
 export class CardComponent implements OnInit {
     @Input() movie: Movie;
