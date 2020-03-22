59ab8f604c12054a94f8f635a50b2fe2c9fb8665
Alex <radionovalek@yandex.ru>
2020-03-23 00:08:22 +0700

    setting grid

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { faBookmark } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.scss']
 })
-
 export class HeaderComponent implements OnInit {
+    faBookmark = faBookmark;
     constructor() { }
 
     ngOnInit(): void { }
